4c245e1b102e5d07e057e38f702014e41c5abd54
Evan Gatchell <greygatch@gmail.com>
2017-04-03 14:55:49 -0500

    add babel-watch and basic hapi server.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import util from 'util';
 import bunyan from 'bunyan';
-import request from 'request-promise';
+import rp from 'request-promise';
 import { wuKey } from './config';
 
+const Hapi = require('hapi');
+const server = new Hapi.Server();
 const log = bunyan.createLogger({name: 'weather-app'});
 
 const wuFeatures = [
@@ -19,15 +21,16 @@ const wuFeatures = [
   'tide'
 ];
 
-function handleError(err) {
-  log.info(err);
-}
+// function handleError(err) {
+//   log.info(err);
+// }
 
-function handleResponse(data) {
-  const parsedData = JSON.parse(data);
-  log.info(`Today is ${new Date().toLocaleString()}`);
-  log.info(parsedData.forecast.simpleforecast);
-}
+// function handleResponse(data) {
+//   const parsedData = JSON.parse(data);
+//   debugger;
+//   log.info(`Today is ${new Date().toLocaleString()}`);
+//   log.info(parsedData.forecast.simpleforecast);
+// }
 
 function formatCity(str) {
   return str.toUpperCase().split(' ').join('_');
@@ -37,17 +40,39 @@ function formatState(str) {
   return str.toUpperCase();
 }
 
-async function getWeatherData(type, city, state) {
+async function getWeatherData(request, reply) {
+  const { type, city, state } = request.url.query;
   const formattedCity = formatCity(city);
   const formattedState = formatState(state);
 
   try {
-    const data = await request(`http://api.wunderground.com/api/${wuKey}/${type}/q/${formattedState}/${formattedCity}.json`);
-    handleResponse(data);
+    const data = await rp(`http://api.wunderground.com/api/${wuKey}/${type}/q/${formattedState}/${formattedCity}.json`);
+    // handleResponse(data);
+    reply(data);
   } catch(err) {
-    handleError(err)
+    // handleError(err)
+    reply('Error fetching data.');
   }
 
 }
 
-getWeatherData(wuFeatures[3], 'san francisco', 'ca')
+server.connection({ port: 3000, host: 'localhost' });
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply('Hello, world!');
+  }
+});
+
+server.route({
+  method: 'GET',
+  path: '/location',
+  handler: getWeatherData
+});
+
+server.start((err) => {
+  if (err) { throw err; }
+  console.log(`Server running at: ${server.info.uri}`);
+});
